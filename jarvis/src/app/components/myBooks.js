c32c65aa34cf6ad2d26f21acbffaecce1e228e22
ADHIKAMSETTY Chaitanya <chaitanya.adhikamsetty@hexagon.com>
2017-10-09 15:55:34 +0530

    designed the page using bootstrap react

--- jarvis/src/app/components/myBooks.js
+++ jarvis/src/app/components/myBooks.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import h from 'react-hyperscript'; 
+import h from 'react-hyperscript';
+import { Form, Button, ControlLabel, FormGroup, FormControl, HelpBlock } from 'react-bootstrap';
 
+export class MyBooks extends React.Component {
 
-export class MyBooks extends React.Component { 
     render() {
         return (
-            h('div', [
-                h('h3', 'This is My Books page') 
-            ]) 
+            h(Form, { inline: true }, [
+                h(FormGroup, { controlId: 'formBasicText' }, [
+                    h(ControlLabel, "Book Name"),    
+                    h(FormControl, { type: 'text', placeholder: "Enter text " })
+                ]),
+                h(FormGroup, { controlId: 'formInlineEmail' }, [
+                    h(ControlLabel, "Author "),
+                    h(FormControl, { type: 'text', placeholder: "Enter text" })
+                ]),
+                h(Button,"save")
+            ])
         );
     }
 }
+
+ 
