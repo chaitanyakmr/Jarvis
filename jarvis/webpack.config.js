e4f96c5d41cf0d2e21df73e66d8c2a7aab292a17
ADHIKAMSETTY Chaitanya <chaitanya.adhikamsetty@hexagon.com>
2017-10-09 10:54:13 +0530

    configured the postcss

--- jarvis/webpack.config.js
+++ jarvis/webpack.config.js
@@ -1,10 +1,11 @@
 var webpack = require("webpack");
-var path = require("path");
+var path = require("path"); 
 
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
+    devtool: 'source-map',
     entry: SRC_DIR + "/app/index.js",
     output: {
         path: DIST_DIR + "/app",
@@ -12,14 +13,27 @@ var config = {
         publicPath: "/app/"
     },
     module: {
-        loaders: [{
-            test: /\.js?/,
-            include: SRC_DIR,
-            loader: "babel-loader",
-            query: {
-                presets: ["react", "env", "stage-2"]
-            }
-        }] 
+        rules: [ 
+            {
+                test: /\.scss$/,
+                use: [
+                  { loader: 'style-loader', options: { sourceMap: true } },
+                  { loader: 'css-loader', options: { sourceMap: true } },
+                  { loader: 'postcss-loader', options: { sourceMap: true } },
+                  'resolve-url-loader',
+                  'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                include: SRC_DIR,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ["react", "env", "stage-2"]
+                }
+              }
+        ]
     }
 }
 
